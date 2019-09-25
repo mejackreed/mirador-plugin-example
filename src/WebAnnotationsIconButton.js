b551e46d7dc115ddbdf000498815df15932cecd1
GroovinChip <groovinchip@gmail.com>
2019-09-25 15:13:43 -0400

    move network call into IconButton file, show JSON response on button click

--- src/WebAnnotationsIconButton.js
+++ src/WebAnnotationsIconButton.js
@@ -2,25 +2,48 @@ import React from "react";
 import MaterialIcon from '@material/react-material-icon';
 import './WebAnnotationsIconButtonStyles.css';
 
-//TODO: add network call to WA endpoint
 class WebAnnotationsIconButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
-
-        // This binding is necessary to make `this` work in the callback
+        this.state = { responseText: Object };
+        this.fetchWebAnnotations = this.fetchWebAnnotations.bind(this);
         this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchWebAnnotations();
+    }
+
+    fetchWebAnnotations() {
+        fetch(
+            "https://rosetest.library.jhu.edu/rosademo/wa/rose/SeldenSupra57/1r/canvas",
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json"
+                }
+            }
+        ).then(response => {
+            if (response.ok) {
+                response.json().then(json => {
+                    this.setState({
+                        responseText: json
+                    });
+                });
+            }
+        });
     }
 
     // Handle button click
     handleClick() {
-        alert('This button will display Web Annotations soon! It will also be moved to the window sidebar.');
+        //alert('This button will display Web Annotations soon! It will also be moved to the window sidebar.');
+        alert(JSON.stringify(this.state.responseText))
     }
 
     render() {
         return (
-            <div id="WebAnnoContainer" class="tooltip">
-                <MaterialIcon icon='class' onClick={this.handleClick} />
+            <div id="WebAnnoContainer" class="tooltip" onClick={this.handleClick}>
+                <MaterialIcon icon='class' />
                 <span class="tooltiptext">Web Annotations</span>
             </div>
         );
