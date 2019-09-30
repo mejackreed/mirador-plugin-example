f27902b3195954ebbbe3d98adffdbfa01df13fbc
GroovinChip <groovinchip@gmail.com>
2019-09-30 09:55:07 -0400

    groundwork for sidebar panel

---
+++ src/WebAnnotationsSideBarPanel.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class WebAnnotationsSideBarPanel extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div className="WebAnnotationsPanel">
+                
+            </div>
+        );
+    }
+}
+
+export default WebAnnotationsSideBarPanel;
