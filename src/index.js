4d0870442aceb99d6ee21a77375d71a268cf592c
GroovinChip <groovinchip@gmail.com>
2019-11-11 15:14:06 -0500

    add other manifests

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ ReactDOM.render(
         manifestId: 'https://rosetest.library.jhu.edu/rosademo/iiif/rose/SeldenSupra57/manifest', 
         canvasId: 'https://rosetest.library.jhu.edu/rosademo/iiif/rose/SeldenSupra57/1r/canvas' 
       }] ,
+    manifests: {
+      'https://rosetest.library.jhu.edu/rosademo/iiif/rose/CoxMacro/manifest': {},
+      'https://rosetest.library.jhu.edu/rosademo/iiif/rose/Douce195/manifest': {},
+      'https://rosetest.library.jhu.edu/rosademo/iiif/rose/Douce332/manifest': {},
+      'https://rosetest.library.jhu.edu/rosademo/iiif/rose/LudwigXV7/manifest': {},
+      'https://rosetest.library.jhu.edu/rosademo/iiif/rose/Morgan948/manifest': {},
+      'https://rosetest.library.jhu.edu/rosademo/iiif/rose/SeldenSupra57/manifest': {},
+      'https://rosetest.library.jhu.edu/rosademo/iiif/rose/Walters143/manifest': {},
+      'https://rosetest.library.jhu.edu/rosademo/iiif/homer/VA/manifest': {}
+    },
     themes:{
        dark: {
           palette: {
