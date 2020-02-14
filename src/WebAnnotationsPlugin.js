a250c8dce0d4e93f99d53cac3597ce870d9486c4
GroovinChip <groovinchip@gmail.com>
2020-02-14 16:17:49 -0500

    start experimenting with redux store

--- src/WebAnnotationsPlugin.js
+++ src/WebAnnotationsPlugin.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import isEqual from 'lodash/isEqual'
 import { getVisibleCanvases } from 'mirador/dist/es/src/state/selectors/canvases'
+import { receiveAnnotation } from 'mirador/dist/es/src/state/actions/annotation'
 import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
@@ -64,6 +65,10 @@ class WebAnnotationsPlugin extends React.Component {
   // that points to our Web Annotation endpoint. If there are no 
   // annotations for a given canvas, we display a message stating so.
   fetchAnnotations(canvases) {
+    const { receiveAnnotation } = this.props;
+    if (!canvases) {
+      return
+    }
     canvases.forEach(canvas => {
       if (canvas) {
         var iiifUrl = canvas.id;
@@ -88,7 +93,9 @@ class WebAnnotationsPlugin extends React.Component {
         fetch(url, {
           method: 'GET',
         }).then(res => res.json())
-          .then((results) => {          
+          .then((results) => { 
+            receiveAnnotation(canvas.id, url, results)   
+            console.log(receiveAnnotation(canvas.id, url, results))      
             if (results.body != undefined) {
               this.setState({
                 body: results.body[0].value
@@ -133,34 +140,31 @@ class WebAnnotationsPlugin extends React.Component {
     }
   }
 
-  // Turn the Web Annotation's transcription into viewable HTML
+  // 
   render() {
     return (
-      <div>
-        <Button onClick={this.openDialog.bind(this)} >View Web Annotations</Button>
-        <Dialog open={this.state.open} onClose={this.closeDialog.bind(this)}>
-        <DialogTitle id="customized-dialog-title" onClose={this.closeDialog.bind(this)}>Web Annotations</DialogTitle>
-          <DialogContent>
-            <Box>{ReactHtmlParser(this.state.body)}</Box>
-          </DialogContent>
-        </Dialog>
-      </div> 
+      <this.props.TargetComponent {...this.props.targetProps}></this.props.TargetComponent>
     )
   }
 } 
 
 // Hook into Mirador's state to get the canvases
-function mapStateToProps(state, { windowId }) {
+function mapStateToProps(state, props) {
   return {
-    canvases: getVisibleCanvases(state, { windowId: windowId }),
+    canvases: getVisibleCanvases(state, { windowId: props.targetProps.windowId }),
     config: state.config,
   };
 };
 
+const mapDispatchToProps = {
+  receiveAnnotation: receiveAnnotation
+}
+
 export default {
   name: "WebAnnotationsPlugin",
-  target: "WindowTopBarPluginMenu",
-  mode: "add",
+  target: "WindowCanvasNavigationControls",
+  mode: "wrap",
   component: WebAnnotationsPlugin,
   mapStateToProps: mapStateToProps,
+  mapDispatchToProps: mapDispatchToProps,
 };
